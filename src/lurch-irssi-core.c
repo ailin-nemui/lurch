d45e2d9a0f405f04b9b491a0297905c66fa6ff41
Ailin Nemui <ailin@z30a.localdomain>
2018-10-16 19:12:05 +0200

    move load and unload functions, introduce PREFS_ADD_... macros

--- src/lurch-irssi-core.c
+++ src/lurch-irssi-core.c
@@ -30,6 +30,10 @@
 #define PREFS_GET_BOOL(prefname) settings_get_bool(prefname)
 #define PREFS_GET_INT(prefname) ( settings_get_choice(prefname) - 1 )
 
+#define PREFS_ADD_NONE(prefname) /* not needed */
+#define PREFS_ADD_BOOL(prefname, default_value) settings_add_bool("misc", prefname, default_value)
+#define PREFS_ADD_CHOICE(prefname, default_value, choice_list) settings_add_choice("misc", prefname, default_value + 1, choice_list);
+
 #define debug_info(a, b, ...) \
   if (log_level >= AXC_LOG_INFO)					\
     printtext(NULL, NULL, MSGLEVEL_CLIENTCRAP, "[" a "][INFO] " b , ##__VA_ARGS__)
@@ -120,3 +124,239 @@ static char * lurch_get_account(SERVER_REC * server, WI_ITEM_REC * item)
 
   return g_strconcat(server->connrec->username, "@", server->tag, NULL);
 }
+
+static void irssi_lurch_peppublish(SERVER_REC * server, const char * from, const char * xml)
+{
+  LmMessage * msg = lm_message_new_with_sub_type(NULL, LM_MESSAGE_TYPE_IQ, LM_MESSAGE_SUB_TYPE_SET);
+  LmMessageNode * pubsub_node_p = lm_message_node_add_child(msg->node, "pubsub", NULL);
+  lm_message_node_set_attribute(pubsub_node_p, "xmlns", "http://jabber.org/protocol/pubsub");
+
+  lm_message_node_set_raw_mode(pubsub_node_p, TRUE);
+  lm_message_node_set_value(pubsub_node_p, xml);
+
+  signal_emit("xmpp send iq", 2, server, msg);
+
+  lm_message_unref(msg);
+}
+
+static void irssi_lurch_send(SERVER_REC * server, const char * from, const char * xml)
+{
+  mxml_node_t * xml_p = (void *) 0;
+
+  xml_p = mxmlLoadString((void *) 0, xml, MXML_OPAQUE_CALLBACK);
+
+  //g_warning("sending xml: ##%s##", xml);
+
+  int lm_type = LM_MESSAGE_SUB_TYPE_NOT_SET;
+  const char * type = mxmlElementGetAttr(xml_p, "type");
+  if (!g_strcmp0("chat", type)) {
+    lm_type = LM_MESSAGE_SUB_TYPE_CHAT;
+  } else if (!g_strcmp0("groupchat", type)) {
+    lm_type = LM_MESSAGE_SUB_TYPE_GROUPCHAT;
+  } else if (!g_strcmp0("headline", type)) {
+    lm_type = LM_MESSAGE_SUB_TYPE_HEADLINE;
+  } else {
+    debug_warning("lurch", "irssi_lurch_send: unknown sub-type: %s", type);
+  }
+
+  LmMessage * msg = lm_message_new_with_sub_type(mxmlElementGetAttr(xml_p, "to"), LM_MESSAGE_TYPE_MESSAGE, lm_type);
+  lm_message_node_set_attribute(msg->node, "id", mxmlElementGetAttr(xml_p, "id"));
+  lm_message_node_set_raw_mode(msg->node, TRUE);
+
+  GString * str = g_string_sized_new(strlen(xml));
+  mxml_node_t * child = mxmlGetFirstChild(xml_p);
+  mxml_node_t * sibling;
+  for (sibling = child; sibling; sibling = mxmlGetNextSibling(sibling)) {
+    mxml_node_t * next = sibling->next;
+    mxml_node_t * parent = sibling->parent;
+    sibling->parent = NULL;
+    sibling->next = NULL;
+    char * frag = mxmlSaveAllocString(sibling, MXML_NO_CALLBACK);
+    g_string_append(str, frag);
+    free(frag);
+    sibling->parent = parent;
+    sibling->next = next;
+  }
+  lm_message_node_set_value(msg->node, str->str);
+  mxml_node_t * body_node_p = mxmlFindPath(xml_p, "body");
+  if (!body_node_p) {
+    lm_message_node_add_child(msg->node, "body", "[Encrypted with OMEMO]");
+  }
+  char * new_xml = lm_message_node_to_string(msg->node);
+  //g_warning("I want to send ##%s##", new_xml);
+  g_free(new_xml);
+  signal_emit("xmpp send message", 2, server, msg);
+
+  g_string_free(str, TRUE);
+  lm_message_unref(msg);
+  mxmlRelease(xml_p);
+}
+
+static void irssi_lurch_peprequest(SERVER_REC * server, const char * to, const char * node, void * callback_fn)
+{
+  LmMessage * jiq_p = lm_message_new_with_sub_type(to, LM_MESSAGE_TYPE_IQ, LM_MESSAGE_SUB_TYPE_GET);
+  LmMessageNode * pubsub_node_p = lm_message_node_add_child(jiq_p->node, "pubsub", NULL);
+  lm_message_node_set_attribute(pubsub_node_p, "xmlns", "http://jabber.org/protocol/pubsub");
+
+  char * req_id;
+  while (1) {
+    char * rand_str = g_strdup_printf("%d", g_random_int());
+    req_id = g_strconcat(to, "#", rand_str, NULL);
+    if (g_hash_table_lookup(lurch_peprequest_response_ht, req_id) != NULL) {
+      g_free(rand_str);
+      g_free(req_id);
+      continue;
+    }
+    break;
+  }
+
+  LmMessageNode * items_node_p = lm_message_node_add_child(pubsub_node_p, "items", NULL);
+  lm_message_node_set_attribute(items_node_p, "node", node);
+  lm_message_node_set_attribute(items_node_p, "max_items", "1");
+
+  lm_message_node_set_attribute(jiq_p->node, "id", req_id);
+
+  g_hash_table_insert(lurch_peprequest_response_ht, req_id, callback_fn);
+
+  signal_emit("xmpp send iq", 2, server, jiq_p);
+
+  lm_message_unref(jiq_p);
+}
+
+void irssi_lurch_read_settings(void)
+{
+  log_level = settings_get_choice(LURCH_PREF_AXC_LOGGING_LEVEL) - 1;
+}
+
+/**
+ * Actions to perform on plugin load.
+ * Inits the crypto and registers signal and PEP handlers.
+ */
+void lurch_core_init(void)
+{
+  GSList * tmp;
+  int ret_val = 0;
+  char * err_msg_dbg = (void *) 0;
+
+  char * dl_ns = (void *) 0;
+  GList * accs_l_p = (void *) 0;
+
+  lurch_bundle_request_ht = g_hash_table_new_full(g_str_hash, g_str_equal, g_free, NULL);
+  lurch_peprequest_response_ht = g_hash_table_new_full(g_str_hash, g_str_equal, g_free, NULL);
+
+  lurch_plugin_load_generic();
+
+  ret_val = omemo_devicelist_get_pep_node_name(&dl_ns);
+  if (ret_val) {
+    err_msg_dbg = "failed to get devicelist pep node name";
+    goto cleanup;
+  }
+
+  settings_add_bool("misc", LURCH_PREF_AXC_LOGGING, TRUE);
+  settings_add_choice("misc", LURCH_PREF_AXC_LOGGING_LEVEL, 5, "none;error;warning;notice;info;debug");
+
+  expando_create("encryption_omemo", (EXPANDO_FUNC) lurch_expando_encryption_omemo,
+		 "window changed", EXPANDO_ARG_NONE,
+		 "window item changed", EXPANDO_ARG_WINDOW,
+		 "lurch encryption changed", EXPANDO_ARG_WINDOW_ITEM,
+		 NULL);
+
+  command_bind("lurch", NULL, (SIGNAL_FUNC) lurch_cmd_func);
+  signal_add("xmpp recv message", (SIGNAL_FUNC) lurch_xml_received_cb);
+  signal_add("xmpp send message", (SIGNAL_FUNC) lurch_xml_sent_cb);
+
+  signal_add("xmpp recv iq", (SIGNAL_FUNC) irssi_lurch_bundle_request_cb);
+  signal_add("xmpp recv iq", (SIGNAL_FUNC) irssi_lurch_peprequest_cb);
+  signal_add("xmpp recv message", (SIGNAL_FUNC) irssi_lurch_message_cb);
+  signal_add("xmpp recv iq", (SIGNAL_FUNC) irssi_lurch_iq_cb);
+
+  // too early :(
+  //signal_add("chat protocol created", (SIGNAL_FUNC) irssi_lurch_register_features_cb);
+
+  signal_emit("xmpp register feature", 1, dl_ns);
+  char * dl_ns_notify = g_strconcat(dl_ns , "+notify", NULL);
+  signal_emit("xmpp register feature", 1, dl_ns_notify);
+  g_free(dl_ns_notify);
+
+  // jabber_pep_register_handler(dl_ns, lurch_pep_devicelist_event_handler);
+  g_dl_ns = g_strdup(dl_ns);
+  // jabber_add_feature(dl_ns, jabber_pep_namespace_only_when_pep_enabled_cb);
+
+  // manually call init code if there are already accounts connected, e.g. when plugin is loaded manually
+  for (tmp = servers; tmp != NULL; tmp = tmp->next) {
+    SERVER_REC * server = tmp->data;
+    if (server->connected) {
+      lurch_account_connect_cb(server);
+    }
+  }
+
+  // register install callback
+  signal_add("server connected", (SIGNAL_FUNC) lurch_account_connect_cb);
+  signal_add_last("query created", (SIGNAL_FUNC) lurch_conv_created_cb);
+  signal_add_last("channel created", (SIGNAL_FUNC) lurch_conv_created_cb);
+  // (void) purple_signal_connect(purple_conversations_get_handle(), "conversation-updated", plugin_p, PURPLE_CALLBACK(lurch_conv_updated_cb), NULL);
+
+  signal_add("lurch peprequest bundle", (SIGNAL_FUNC) irssi_lurch_peprequest);
+  signal_add("lurch peprequest own_devicelist", (SIGNAL_FUNC) irssi_lurch_peprequest);
+  signal_add("lurch peprequest devicelist", (SIGNAL_FUNC) irssi_lurch_peprequest);
+
+  signal_add("lurch peppublish bundle", (SIGNAL_FUNC) irssi_lurch_peppublish);
+  signal_add("lurch send message", (SIGNAL_FUNC) irssi_lurch_send);
+  signal_add("lurch send keytransport", (SIGNAL_FUNC) irssi_lurch_send);
+  signal_add("lurch peppublish devicelist", (SIGNAL_FUNC) irssi_lurch_peppublish);
+
+  signal_add("setup changed", (SIGNAL_FUNC) irssi_lurch_read_settings);
+  irssi_lurch_read_settings();
+
+cleanup:
+  free(dl_ns);
+  g_list_free(accs_l_p);
+  if (ret_val) {
+    debug_error("lurch", "%s: %s (%d)", __func__, err_msg_dbg, ret_val);
+  }
+
+  module_register("lurch", "core");
+}
+
+void lurch_core_deinit(void)
+{
+  command_unbind("lurch", (SIGNAL_FUNC) lurch_cmd_func);
+  signal_remove("xmpp recv message", (SIGNAL_FUNC) lurch_xml_received_cb);
+  signal_remove("xmpp send message", (SIGNAL_FUNC) lurch_xml_sent_cb);
+
+  signal_remove("xmpp recv iq", (SIGNAL_FUNC) irssi_lurch_bundle_request_cb);
+  signal_remove("xmpp recv iq", (SIGNAL_FUNC) irssi_lurch_peprequest_cb);
+  signal_remove("xmpp recv message", (SIGNAL_FUNC) irssi_lurch_message_cb);
+  signal_remove("xmpp recv iq", (SIGNAL_FUNC) irssi_lurch_iq_cb);
+
+  signal_remove("server connected", (SIGNAL_FUNC) lurch_account_connect_cb);
+  signal_remove("query created", (SIGNAL_FUNC) lurch_conv_created_cb);
+  signal_remove("channel created", (SIGNAL_FUNC) lurch_conv_created_cb);
+
+  signal_remove("lurch peprequest bundle", (SIGNAL_FUNC) irssi_lurch_peprequest);
+  signal_remove("lurch peprequest own_devicelist", (SIGNAL_FUNC) irssi_lurch_peprequest);
+  signal_remove("lurch peprequest devicelist", (SIGNAL_FUNC) irssi_lurch_peprequest);
+
+  signal_remove("lurch peppublish bundle", (SIGNAL_FUNC) irssi_lurch_peppublish);
+  signal_remove("lurch send message", (SIGNAL_FUNC) irssi_lurch_send);
+  signal_remove("lurch send keytransport", (SIGNAL_FUNC) irssi_lurch_send);
+  signal_remove("lurch peppublish devicelist", (SIGNAL_FUNC) irssi_lurch_peppublish);
+
+  signal_remove("setup changed", (SIGNAL_FUNC) irssi_lurch_read_settings);
+
+  omemo_default_crypto_teardown();
+
+  expando_destroy("encryption_omemo", (EXPANDO_FUNC) lurch_expando_encryption_omemo);
+
+  g_hash_table_unref(lurch_bundle_request_ht);
+  g_hash_table_unref(lurch_peprequest_response_ht);
+  g_free(g_dl_ns);
+}
+
+#ifdef IRSSI_ABI_VERSION
+void
+lurch_core_abicheck(int * version)
+{
+        *version = IRSSI_ABI_VERSION;
+}
+#endif
--- src/lurch.c
+++ src/lurch.c
@@ -2605,161 +2605,20 @@ cleanup:
 }
 
 /**
- * Actions to perform on plugin load.
- * Inits the crypto and registers signal and PEP handlers.
+ * Generic actions to perform on plugin load.
+ * Inits the crypto.
  */
-static gboolean lurch_plugin_load(PurplePlugin * plugin_p) {
-  int ret_val = 0;
-  char * err_msg_dbg = (void *) 0;
-
-  char * dl_ns = (void *) 0;
-  void * jabber_handle_p = (void *) 0;
-  GList * accs_l_p = (void *) 0;
-  GList * curr_p = (void *) 0;
-  PurpleAccount * acc_p = (void *) 0;
-
+static gboolean lurch_plugin_load_generic(void) {
   omemo_default_crypto_init();
-
-  ret_val = omemo_devicelist_get_pep_node_name(&dl_ns);
-  if (ret_val) {
-    err_msg_dbg = "failed to get devicelist pep node name";
-    goto cleanup;
-  }
-
-  lurch_cmd_id = purple_cmd_register("lurch",
-                                     "wwws",
-                                     PURPLE_CMD_P_PLUGIN,
-                                     PURPLE_CMD_FLAG_IM | PURPLE_CMD_FLAG_CHAT | PURPLE_CMD_FLAG_PRPL_ONLY | PURPLE_CMD_FLAG_ALLOW_WRONG_ARGS,
-                                     JABBER_PROTOCOL_ID,
-                                     lurch_cmd_func,
-                                     "lurch &lt;help&gt;:  "
-                                     "Interface to the lurch plugin. For details, use the 'help' argument.",
-                                     (void *) 0);
-
-  // register handlers
-  jabber_handle_p = purple_plugins_find_with_id(JABBER_PROTOCOL_ID);
-
-  (void) purple_signal_connect_priority(jabber_handle_p, "jabber-receiving-xmlnode", plugin_p, PURPLE_CALLBACK(lurch_xml_received_cb), NULL, PURPLE_PRIORITY_HIGHEST - 100);
-  (void) purple_signal_connect_priority(jabber_handle_p, "jabber-sending-xmlnode", plugin_p, PURPLE_CALLBACK(lurch_xml_sent_cb), NULL, PURPLE_PRIORITY_HIGHEST - 100);
-
-  jabber_pep_register_handler(dl_ns, lurch_pep_devicelist_event_handler);
-  jabber_add_feature(dl_ns, jabber_pep_namespace_only_when_pep_enabled_cb);
-
-  // manually call init code if there are already accounts connected, e.g. when plugin is loaded manually
-  accs_l_p = purple_accounts_get_all_active();
-  for (curr_p = accs_l_p; curr_p; curr_p = curr_p->next) {
-    acc_p = (PurpleAccount *) curr_p->data;
-    if (purple_account_is_connected(acc_p)) {
-      if (!g_strcmp0(purple_account_get_protocol_id(acc_p), JABBER_PROTOCOL_ID)) {
-        lurch_account_connect_cb(acc_p);
-      }
-    }
-  }
-
-  // register install callback
-  (void) purple_signal_connect(purple_accounts_get_handle(), "account-signed-on", plugin_p, PURPLE_CALLBACK(lurch_account_connect_cb), NULL);
-  (void) purple_signal_connect(purple_conversations_get_handle(), "conversation-created", plugin_p, PURPLE_CALLBACK(lurch_conv_created_cb), NULL);
-  (void) purple_signal_connect(purple_conversations_get_handle(), "conversation-updated", plugin_p, PURPLE_CALLBACK(lurch_conv_updated_cb), NULL);
-
-cleanup:
-  free(dl_ns);
-  g_list_free(accs_l_p);
-  if (ret_val) {
-    debug_error("lurch", "%s: %s (%i)", __func__, err_msg_dbg, ret_val);
-    omemo_default_crypto_teardown();
-    return FALSE;
-  }
-
-  return TRUE;
 }
 
-static gboolean lurch_plugin_unload(PurplePlugin * plugin_p) {
+static void lurch_plugin_unload_generic(void) {
   omemo_default_crypto_teardown();
-
-  return TRUE;
 }
-
-static PurplePluginPrefFrame * lurch_get_plugin_pref_frame(PurplePlugin * plugin_p) {
-  PurplePluginPrefFrame * frame_p;
-  PurplePluginPref * ppref_p;
-
-  frame_p = purple_plugin_pref_frame_new();
-  ppref_p = purple_plugin_pref_new_with_label("Extended logging");
-  purple_plugin_pref_frame_add(frame_p, ppref_p);
-
-  ppref_p = purple_plugin_pref_new_with_name_and_label(
-                  LURCH_PREF_AXC_LOGGING,
-                  "Show log output from underlying libraries");
-  purple_plugin_pref_frame_add(frame_p, ppref_p);
-
-  ppref_p = purple_plugin_pref_new_with_name_and_label(
-                    LURCH_PREF_AXC_LOGGING_LEVEL,
-                    "Log level");
-  purple_plugin_pref_set_type(ppref_p, PURPLE_PLUGIN_PREF_CHOICE);
-  purple_plugin_pref_add_choice(ppref_p, "ERROR", GINT_TO_POINTER(AXC_LOG_ERROR));
-  purple_plugin_pref_add_choice(ppref_p, "WARNING", GINT_TO_POINTER(AXC_LOG_WARNING));
-  purple_plugin_pref_add_choice(ppref_p, "NOTICE", GINT_TO_POINTER(AXC_LOG_NOTICE));
-  purple_plugin_pref_add_choice(ppref_p, "INFO", GINT_TO_POINTER(AXC_LOG_INFO));
-  purple_plugin_pref_add_choice(ppref_p, "DEBUG", GINT_TO_POINTER(AXC_LOG_DEBUG));
-  purple_plugin_pref_frame_add(frame_p, ppref_p);
-
-  return frame_p;
-}
-
-static PurplePluginUiInfo prefs_info = {
-  lurch_get_plugin_pref_frame,
-  0,    /* page_num (Reserved) */
-  NULL, /* frame (Reserved) */
-
-  /* padding */
-  NULL,
-  NULL,
-  NULL,
-  NULL
-};
-
-static PurplePluginInfo info = {
-    PURPLE_PLUGIN_MAGIC,
-    PURPLE_MAJOR_VERSION,
-    PURPLE_MINOR_VERSION,
-    PURPLE_PLUGIN_STANDARD,
-    NULL,
-    0,
-    NULL,
-    PURPLE_PRIORITY_DEFAULT,
-
-    "core-riba-lurch",
-    "lurch",
-    LURCH_VERSION,
-
-    "Implements OMEMO for libpurple.",
-    "End-to-end encryption using the Signal protocol, adapted for XMPP.",
-    LURCH_AUTHOR,
-    "https://github.com/gkdr/lurch",
-
-    lurch_plugin_load,
-    lurch_plugin_unload,
-    NULL,
-
-    NULL,
-    NULL,
-    &prefs_info, // plugin config, see libpurple/plugins/pluginpref_example.c
-    NULL,        // plugin actions, see https://developer.pidgin.im/wiki/CHowTo/PluginActionsHowTo
-    NULL,
-    NULL,
-    NULL,
-    NULL
-};
 
 static void
-lurch_plugin_init(PurplePlugin * plugin_p) {
-  PurplePluginInfo * info_p = plugin_p->info;
-
-  info_p->dependencies = g_list_prepend(info_p->dependencies, "prpl-jabber");
-
-  purple_prefs_add_none(LURCH_PREF_ROOT);
-  purple_prefs_add_bool(LURCH_PREF_AXC_LOGGING, FALSE);
-  purple_prefs_add_int(LURCH_PREF_AXC_LOGGING_LEVEL, AXC_LOG_INFO);
+lurch_plugin_init_generic(void) {
+  PREFS_ADD_NONE(LURCH_PREF_ROOT);
+  PREFS_ADD_BOOL(LURCH_PREF_AXC_LOGGING, FALSE);
+  PREFS_ADD_CHOICE(LURCH_PREF_AXC_LOGGING_LEVEL, AXC_LOG_INFO, "none;error;warning;notice;info;debug");
 }
-
-PURPLE_INIT_PLUGIN(lurch, lurch_plugin_init, info)
